3bc86557cbdffbc94ecd4752ae542877dd02b249
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-02 17:28:37 +0930

    test: add initial test

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const should = require('chai').should(); // eslint-disable-line
+
+describe('hexo-notify', () => {
+  // Tests.
+  it('should notify.', () => {
+    return true;
+  });
+});
